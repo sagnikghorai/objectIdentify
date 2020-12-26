28d3263b6735885d33a4219abcb4d15e9ffbbdb5
sagnikghorai <63553430+sagnikghorai@users.noreply.github.com>
2020-12-26 14:22:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-img = "";
+
 objects = [];
 status = "";
-
-function preload(){
-  img = loadImage('dog_cat.jpg');
-}
 
 
 function setup() {
@@ -13,13 +9,18 @@ function setup() {
   video = createCapture(VIDEO);
   video.size(380,380);
   video.hide();
-  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-  document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 
 function modelLoaded() {
   console.log("Model Loaded!")
   status = true;
+}
+
+function start()
+{
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+  object_name = document.getElementById("object_name").value;
 }
 
 function gotResult(error, results) {
@@ -30,25 +31,35 @@ function gotResult(error, results) {
   objects = results;
 }
 
-
 function draw() {
   image(video, 0, 0, 380, 380);
       if(status != "")
       {
-        r =  random(255);
-        g =  random(255);
-        b =  random(255);      
         objectDetector.detect(video, gotResult);
-        for (i = 0; i < objects.length; i++) {
+        for (i = 0; i < objects.length; i++) {gt
           document.getElementById("status").innerHTML = "Status : Object Detected";
-          document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
- 
-          fill(r,g,b);
+          
+          fill("#FF0000");
           percent = floor(objects[i].confidence * 100);
           text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
           noFill();
-          stroke(r,g,b);
+          stroke("#FF0000");
           rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
-        }
+
+         
+          if(objects[i].label == object_name)
+          {
+            video.stop();
+            objectDetector.detect(gotResult);
+            document.getElementById("object_status").innerHTML = object_name + " Found";
+            synth = window.speechSynthesis;
+            utterThis = new SpeechSynthesisUtterance(object_name + "Found");
+            synth.speak(utterThis);
+          }
+          else
+          {
+            document.getElementById("object_status").innerHTML = object_name + " Not Found";
+          }          
+         }
       }
 }
